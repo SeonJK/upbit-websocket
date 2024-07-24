dfbb614fddbb7745cbd3a2d99943a967da17a66d
SeonJK <tjswhdrbs14@naver.com>
2024-07-25 00:28:58 +0900

    initial commit

---
+++ app/src/main/java/com/seonjk/upbit_websocket/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.seonjk.upbit_websocket.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seonjk.upbit_websocket.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
